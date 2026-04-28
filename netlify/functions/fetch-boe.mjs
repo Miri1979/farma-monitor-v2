99d322d55a2793f248220ff8795929ac667b46ed
Miri1979 <mireia_castro_roman@yahoo.es>
2026-04-28 09:04:24 +0200

    Update fetch-boe.mjs

--- netlify/functions/fetch-boe.mjs
+++ netlify/functions/fetch-boe.mjs
@@ -1,7 +1,1 @@
-let isToday = true;
-if (pubDate) {
-  const itemDate = new Date(pubDate).toISOString().slice(0, 10);
-  isToday = itemDate === today;
-}
-
-if (isToday && title && link) {
+if (title && link) {
